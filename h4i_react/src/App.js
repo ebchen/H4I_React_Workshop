d1b25fffa1708434343e1976e11eedf2249c6685
ebchen <eric.chen.us@gmail.com>
2020-10-12 11:27:28 -0500

    added taskcard component to app

--- h4i_react/src/App.js
+++ h4i_react/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import React, {useState} from 'react';
+import TaskCard from "./TaskCard";
 
 function App() {
   const [tasks, changeTasks] = useState([{title:"test1"}])
   return (
     <div className="App">
-      <p>{tasks.map(task => <div>{task.title}</div>)}</p>
+      <p>{tasks.map(t => <div><TaskCard task = {t}/></div>)}</p>
     </div>
   );
 }
