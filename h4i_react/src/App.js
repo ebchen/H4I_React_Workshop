571f408c4f7802d79a30bac4e7cb0ecf5a76249a
ebchen <eric.chen.us@gmail.com>
2020-10-12 11:35:06 -0500

    changed css

--- h4i_react/src/App.js
+++ h4i_react/src/App.js
@@ -4,9 +4,10 @@ import React, {useState} from 'react';
 import TaskCard from "./TaskCard";
 
 function App() {
-  const [tasks, changeTasks] = useState([{title:"test1"}])
+  const [tasks, changeTasks] = useState([{title:"Buy groceries"}, {title:"Watch cis120 lecture"}, {title: "Do laundry"}])
   return (
     <div className="App">
+      <h1>To Do List</h1>
       <p>{tasks.map(t => <div><TaskCard task = {t}/></div>)}</p>
     </div>
   );
