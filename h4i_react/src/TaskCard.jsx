fc243c15ffaf1c6dd9f00d9a46522728f42878bf
Mohammed Abaker <mohamme@Mohammeds-MacBook-Air.local>
2020-10-14 11:30:24 -0400

    editable

--- h4i_react/src/TaskCard.jsx
+++ h4i_react/src/TaskCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
@@ -14,18 +14,33 @@ const useStyles = makeStyles((theme) => ({
 
 const Task = ({task, remove, edit, check}) => {
     const classes = useStyles();
+    const [editable, changeMode] = useState(false);
+    const [tempTitle,changeTempTtile] = useState(task.title);
+    const save =() => {
+        edit(task._id, task.title );
+        changeMode(!editable)
+    }
+    const changeTitle = (e) => {
+        changeTempTtile(e.target.value);
+    }
     
    return (<Card className={classes.root}>
         <CardContent>
         <Typography component="p" style={{textAlign:"center", fontSize:"1.7rem"}} >
-            {task.title}
+           {editable ? <input value={tempTitle} onChange = {changeTitle} /> : <> {task.title} </> } 
         </Typography>
         </CardContent>
         <CardActions style={{display:"flex", flexDirection: "row", justifyContent :"space-between", margin:"0 25%"}}>   
-       <div className="flex-Item" style={{cursor:"pointer"}}> <i onClick={()=>{edit(task._id)}} class="fas fa-edit fa-lg"></i></div>
+       {!editable ? 
+       <>
+       <div className="flex-Item" style={{cursor:"pointer"}}> <i onClick={()=>{changeMode(!editable)}} class="fas fa-edit fa-lg"></i></div>
        <div className="flex-Item" style={{cursor:"pointer"}}> <i onClick={()=>{remove(task._id)}} class="fas fa-trash fa-lg"></i> </div> 
        <div className="flex-Item" style={{cursor:"pointer"}}>  {task.checked? (<i onClick={()=>{ check(task._id)}} class="far fa-check-square fa-lg"></i>) : (<i onClick={()=>{ check(task._id)}} class="far fa-square fa-lg"></i>)} </div>
-      </CardActions>
+    </> 
+    :
+    <button onClick = {save} className = "btn btn-primary"> Save </button>
+   }
+       </CardActions>
     </Card>);}
  
 
